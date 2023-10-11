88456d8e5d808eca362474bdcd72d761048c9149
Derek <Sadler_derek@comcast.net>
2023-10-10 23:10:14 -0700

    Added basic program models

--- fintech_testbed_project/fintech_testbed_app/models.py
+++ fintech_testbed_project/fintech_testbed_app/models.py
@@ -1,3 +1,76 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Admin(models.Model):
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4,
+        help_text = 'Unique ID for the admin'
+    )
+    
+    username = models.CharField(
+        max_length=100, 
+        help_text='Enter a admin username'
+    )
+
+    email = models.CharField(
+        max_length=100,
+        help_text='Enter a client email'
+
+    )
+
+    password = models.CharField(
+        max_length=100,
+        help_text='Enter a password'
+    )
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id}'
+
+class Client(models.Model):
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4,
+        help_text = 'Unique ID for the client'
+    )
+    
+    username = models.CharField(
+        max_length=100, 
+        help_text='Enter a client username'
+    )
+
+    email = models.CharField(
+        max_length=100,
+        help_text='Enter a client email'
+
+    )
+
+    password = models.CharField(
+        max_length=100,
+        help_text='Enter a password'
+    )
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id}'
+
+class BankAccount(models.Model):
+    id = models.UUIDField(
+        primary_key=True, 
+        default=uuid.uuid4,
+        help_text = 'Unique ID for the bank account'
+    )
+
+    client_id = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+        null=False,
+        help_text = 'The ID of the client using this account'
+    )
+
+    savings = models.FloatField()
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id}'
