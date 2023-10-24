670b1586f98666d638a4566bbb78520ef32c15cb
Ethan <98137983+emcpheeWSU@users.noreply.github.com>
2023-10-23 19:56:16 -0700

    Added User table to create tables

---
+++ SQL/create_tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    password_hash VARCHAR(60) NOT NULL,
+    password_salt CHAR(16) NOT NULL,
+
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(255) NOT NULL,
+
+    UNIQUE (username),
+    UNIQUE (email),
+
+    balance INT,
+    PRIMARY KEY (user_id)
+);
