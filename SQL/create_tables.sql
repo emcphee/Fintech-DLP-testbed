669659b153d59a81647b311cd2dd9b2df048cfee
Ethan <98137983+emcpheeWSU@users.noreply.github.com>
2023-10-23 19:57:51 -0700

    Fixed typo

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
     password_hash VARCHAR(60) NOT NULL,
     password_salt CHAR(16) NOT NULL,
