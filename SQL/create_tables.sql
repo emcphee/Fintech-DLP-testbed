a9283a812c18351016a331663c99b1127c978f16
Ethan <98137983+emcpheeWSU@users.noreply.github.com>
2023-10-24 20:05:41 -0700

    Changed sql create statements

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -1,16 +1,23 @@
-CREATE TABLE user (
-    user_id INT AUTO_INCREMENT,
+CREATE TABLE users (
+    user_id SERIAL,
     username VARCHAR(50) NOT NULL,
+
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+
     password_hash VARCHAR(60) NOT NULL,
     password_salt CHAR(16) NOT NULL,
-
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    email VARCHAR(255) NOT NULL,
 
     UNIQUE (username),
     UNIQUE (email),
 
-    balance INT,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE financial_account (
+    user_id SERIAL,
+    balance double precision,
+
     PRIMARY KEY (user_id)
 );
